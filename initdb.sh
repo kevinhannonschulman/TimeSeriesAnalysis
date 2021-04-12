f3c113b5a72f77c660b20ec578d18274b359201a
Kevin Hannon Schulman <kevinhannon95@gmail.com>
2021-04-12 14:05:05 -0400

    Initial commit

---
+++ initdb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB"  <<-EOSQL
+    create schema if not exists $SCHEMA;
+    create table $SCHEMA.my_table (
+  Row_ID        INTEGER  NOT NULL PRIMARY KEY 
+  ,Order_ID      VARCHAR(14) NOT NULL
+  ,Order_Date    DATE  NOT NULL
+  ,Ship_Date     DATE  NOT NULL
+  ,Ship_Mode     VARCHAR(14) NOT NULL
+  ,Customer_ID   VARCHAR(8) NOT NULL
+  ,Customer_Name VARCHAR(22) NOT NULL
+  ,Segment       VARCHAR(11) NOT NULL
+  ,Country       VARCHAR(13) NOT NULL
+  ,City          VARCHAR(17) NOT NULL
+  ,State         VARCHAR(20) NOT NULL
+  ,Postal_Code   INTEGER 
+  ,Region        VARCHAR(7) NOT NULL
+  ,Product_ID    VARCHAR(15) NOT NULL
+  ,Category      VARCHAR(15) NOT NULL
+  ,SubCategory   VARCHAR(11) NOT NULL
+  ,Product_Name  VARCHAR(127) NOT NULL
+  ,Sales         NUMERIC(9,4) NOT NULL
+        );
+EOSQL
+
