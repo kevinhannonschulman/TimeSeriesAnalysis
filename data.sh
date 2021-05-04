e70f3a67d277fa50775b10ded2a6f2ff32d4de3e
Kevin Hannon Schulman <kevinhannon95@gmail.com>
2021-05-04 14:48:37 -0400

    May 4

--- data.sh
+++ data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB"  <<-EOSQL
-    COPY $SCHEMA.my_table FROM '/data/SuperstoreSalesData.csv' DELIMITER ',' CSV HEADER;
+    COPY $SCHEMA.my_table FROM '/data/TSA_Dataset.csv' DELIMITER ',' CSV HEADER;
 EOSQL
--- initdb.sh
+++ initdb.sh
@@ -2,22 +2,20 @@
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB"  <<-EOSQL
     create schema if not exists $SCHEMA;
     create table $SCHEMA.my_table (
-        order_id text,
-        order_date text,
-        ship_date text,
-        ship_mode text,
-        customer_id text,
-        customer_name text,
-        segment text,
-        country text,
-        city text,
-        state text,
-        postal_code text,
-        region text,
-        product_id text,
-        category text,
-        subcategory text,
-        product_name text,
-        sales text	
-        );
+        date text,
+        total text,
+        anc text,
+        atl text,
+        den text,
+        dfw text,
+        fll text,
+        las text,
+        lax text,
+        mia text,
+        mco text,
+        pdx text,
+        phx text,
+        sea text,
+        sfo text,
+        tpa text);
 EOSQL
